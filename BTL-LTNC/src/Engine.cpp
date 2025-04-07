60eca1796e2da08c5c645c40909b077ed44d7dc5
Doanh36 <doanhabcxyz2006@gmail.com>
2025-04-07 11:48:18 +0700

    draw maze 11x20 and draw pacman can open his mouth

---
+++ BTL-LTNC/src/Engine.cpp
@@ -0,0 +1,97 @@
+#include "Engine.h"
+#include "TextureManager.h"
+#include "Vector2D.h"
+#include "Transform.h"
+
+
+Engine* Engine::s_Instance = nullptr;
+int pacmanFrame = 0;
+Uint32 lastAnimTime = 0;
+const int animDelay = 150; // đổi frame mỗi 150ms
+
+
+bool Engine::Init(){
+    if ( SDL_Init(SDL_INIT_VIDEO) != 0 && IMG_Init( IMG_INIT_JPG | IMG_INIT_PNG ) ) {
+        SDL_Log( "Failed to initialize SDL: %s" , SDL_GetError() ) ;
+        return false;
+    }
+
+    m_Window = SDL_CreateWindow( " Pac-man " , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , SCREEN_WIDTH , SCREEN_HEIGHT ,0);
+    if ( m_Window == nullptr ) {
+        SDL_Log( "Failed to create Window: %s" , SDL_GetError() ) ;
+        return false;
+    }
+
+    m_Renderer = SDL_CreateRenderer( m_Window , -1 , SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+    if ( m_Renderer == nullptr ) {
+        SDL_Log( "Failed to create Renderer: %s" , SDL_GetError() ) ;
+        return false;
+    }
+
+    TextureManager::GetInstance()->Load( "pacman" , "assets/pacman.png");
+
+
+    Transform tf;
+    tf.Log();
+
+    gameMap = new Map();
+
+    return m_IsRunning = true;
+}
+
+void Engine::Update(){
+    if (SDL_GetTicks() - lastAnimTime > animDelay) {
+    pacmanFrame = (pacmanFrame + 1) % 2; // 2 frame: 0 và 1
+    lastAnimTime = SDL_GetTicks();
+    }
+    //SDL_Log("SDL is working");
+    //SDL_Delay(100);
+}
+
+void Engine::Render(){
+    SDL_SetRenderDrawColor( m_Renderer, 0 , 0 , 0 , 0 );
+    SDL_RenderClear(m_Renderer);
+
+    gameMap->Draw(m_Renderer);
+
+    TextureManager::GetInstance()->DrawFrame(
+    "pacman",      // id của texture bạn đã load từ trước
+    32, 29,
+    32, 32,        // width, height mỗi frame
+    0,             // row 0 (nằm ngang)
+    pacmanFrame,   // frame hiện tại (0 hoặc 1)
+    SDL_FLIP_NONE
+    );
+
+
+    //TextureManager::GetInstance()->Draw( "pacman", 32, 29, 32, 32 );
+    SDL_RenderPresent(m_Renderer);
+}
+
+void Engine::Events(){
+    SDL_Event event;
+
+
+
+    SDL_PollEvent ( &event );
+
+    switch ( event.type ) {
+        case SDL_QUIT:;
+        Quit();
+        break;
+    }
+}
+
+bool Engine::Clean(){
+    TextureManager::GetInstance()->Clean();
+    delete gameMap;
+    SDL_DestroyRenderer ( m_Renderer );
+    SDL_DestroyWindow ( m_Window );
+    IMG_Quit();
+    SDL_Quit();
+
+}
+
+void Engine::Quit(){
+    m_IsRunning = false;
+}
